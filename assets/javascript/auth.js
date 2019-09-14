3e60ef625f6dd50426cbb2b2bded2face1baea08
Jordan McCollam <jordy.mccollam@gmail.com>
2019-09-14 11:57:59 -0500

    started generator / no images involved yet

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDDLf6v9gPxeQsmHLN5NCfCJl1MymsTorQ",
-    authDomain: "clothing-generator.firebaseapp.com",
-    databaseURL: "https://clothing-generator.firebaseio.com",
-    projectId: "clothing-generator",
-    storageBucket: "clothing-generator.appspot.com",
-    messagingSenderId: "4788291125",
-    appId: "1:4788291125:web:4f7ef8d32b277d7b27137c"
-  };
+  apiKey: "AIzaSyDDLf6v9gPxeQsmHLN5NCfCJl1MymsTorQ",
+  authDomain: "clothing-generator.firebaseapp.com",
+  databaseURL: "https://clothing-generator.firebaseio.com",
+  projectId: "clothing-generator",
+  storageBucket: "clothing-generator.appspot.com",
+  messagingSenderId: "4788291125",
+  appId: "1:4788291125:web:4f7ef8d32b277d7b27137c"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,46 @@
 $(document).ready(function() {
 
-    var uploadInput=$("#upload-input");
-    var uploadBtn=$("#upload-btn");
-    uploadBtn.on("click",function(){
-    uploadInput.click();
-        
+    var inventory = [];
+    var top = [];
+    var bottom = [];
 
+    // Upload
+    // var uploadInput=$("#upload-input");
+    // var uploadBtn=$("#upload-btn");
+    // uploadBtn.on("click",function(){
+    //     uploadInput.click();
+
+    //     var image = $("#upload-input").val();
+    //     inventory.push(image);
+    //     console.log(image);
+    // })
+
+
+
+    $("#submit").on('click', function() {
+        event.preventDefault();
+
+        var itemName = $("#item-name").val();
+
+        if (document.getElementById("item-top").classList.contains("active") == true) {
+            inventory.push(itemName);
+            top.push(itemName);
+            $(".inventory-display").append("<p>" + itemName);
+        }
+        else if (document.getElementById("item-bottom").classList.contains("active") == true) {
+            inventory.push(itemName);
+            bottom.push(itemName);
+            $(".inventory-display").append("<p>" + itemName);
+        }
     })
+
+    $("#generate-inventory").on('click', function() {
+        var randomTop = top[Math.floor(Math.random() * top.length)];
+        var randomBottom = bottom[Math.floor(Math.random() * bottom.length)];
+
+        $(".outfit-display").html("<p>" + randomTop + "<p>" + randomBottom);
+    })
+
 
     
 
