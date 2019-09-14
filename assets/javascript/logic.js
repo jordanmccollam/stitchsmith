466e0fba37d1f79f5d6a7799260c4e96b7b45319
Brianna Psanos <bpsanos@yahoo.com>
2019-09-14 10:35:37 -0500

    upload btn works

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    var uploadInput=$("#upload-input");
+    var uploadBtn=$("#upload-btn");
+    uploadBtn.on("click",function(){
+    uploadInput.click();
+        
+
+    })
+
     
 
 // END of jQuery
