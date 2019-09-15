f7ab9b4a20b97ddc65b79e4d3abf39decbe2cdc6
Jordan McCollam <jordy.mccollam@gmail.com>
2019-09-14 20:30:39 -0500

    fixed weather services / BUG: still can't toggle off

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
 
+    var weatherServices = "off";
+    var hot = false;
+
     // Prompt user
-    $(".switch").on('click', function() {
-        $('#location-prompt').modal("show");
+
+    $(".normal").on('click', function() {
+        toggleOn();
     })
+    function toggleOn() {
+        $(".normal").on('click', function() {
+            $('#location-prompt').modal("show");
+            $(".switch").removeClass("normal");
+            $(".switch").addClass("toggled");
+            // toggleOff();
+        })
+    }
 
     $("#location-submit").on('click', function() {
         event.preventDefault();
         var city = $("#city").val();
-        var state = $("#state").val();
         var country = $("#country").val();
-        configureSettings(city, state, country);
+        // configureSettings(city, state, country);
+        weather(city, country);
         $('#location-prompt').modal("hide");
+        weatherServices = "on";
     })
 
     var type ={
@@ -35,35 +48,66 @@ $(document).ready(function() {
              "assets/icons/pants.png",
               "assets/icons/skirts.png"
             ],
+        hotTops: [
+            "assets/icons/shortTs.png",
+              "assets/icons/baseballTs.png",
+              "assets/icons/graphicTs.png",
+        ],
+        hotBottoms: [
+            "assets/icons/shorts.png",
+              "assets/icons/skirts.png"
+        ],
+        coldBottoms: [
+            "assets/icons/pants.png",
+        ],
     };
+
+    function weather(city, country) {
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=imperial&APPID=718adad55220d55bd05121a7873d51c6";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            var temp = response.main.temp;
+            console.log("Current Temp: " + temp);
+
+            if (temp >= 80) {
+                hot = true;
+            } else {
+                hot = false;
+            }
+        })
+    }
 
     $("#generate-btn").on('click', function() {
         generateType();
     })
     function generateType() {
-        var randomTop = type.outerwear[Math.floor(Math.random() * type.outerwear.length)];
+        var randomTop = type.tops[Math.floor(Math.random() * type.tops.length)];
+        var randomHotTops = type.hotTops[Math.floor(Math.random() * type.hotTops.length)];
+        var randomOuterwear = type.outerwear[Math.floor(Math.random() * type.outerwear.length)];
+        var randomHotBottom = type.hotBottoms[Math.floor(Math.random() * type.hotBottoms.length)];
+        var randomColdBottom = type.coldBottoms[Math.floor(Math.random() * type.coldBottoms.length)];
         var randomBottom = type.bottoms[Math.floor(Math.random() * type.bottoms.length)];
 
-        $(".top-display").html("<img src='" + randomTop + "'>");
-        $(".bottom-display").html("<img src='" + randomBottom + "'>");
+        // NO weather services
+        if (weatherServices === "off") {
+            $(".top-display").html("<img src='" + randomTop + "'>");
+            $(".bottom-display").html("<img src='" + randomBottom + "'>");
+        }
+        // If it's HOT
+        else if (weatherServices === "on" && hot === true) {
+            $(".top-display").html("<img src='" + randomHotTops + "'>");
+            $(".bottom-display").html("<img src='" + randomHotBottom + "'>");
+        }
+        // If it's NOT hot
+        else if (weatherServices === "on" && hot === false) {
+            $(".top-display").html("<img src='" + randomOuterwear + "'>");
+            $(".bottom-display").html("<img src='" + randomColdBottom + "'>");
+        }
     };
 
-    function configureSettings(city, state, country) {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html&q=" + city + "%2C" + country,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": "4787faa5b5msh72e3d4775f16583p105be1jsn032aa25876b7"
-            }
-        }
-        
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
-    }
 
 
 // END of jQuery
